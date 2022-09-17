d8ddc37a481ed6e5d6f5efccf6f3cf85f18d9fd2
sevbanates <sevban_ates42@hotmail.com>
2022-09-17 10:24:42 +0300

    Add project files.

---
+++ Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Rules
+{
+    public class BrandBusinessRules
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandBusinessRules(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<Brand> result = await _brandRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("SomeFeatureEntity name exists.");
+        }
+
+        public async Task BrandShouldExistWhenRequested(int id)
+        {
+            Brand brand = await _brandRepository.GetAsync(b => b.Id==id);
+            if (brand == null) throw new BusinessException("Requested brand does not exists.");
+        }
+    }
+}
---
+++ Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Repositories;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories
+{
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
